148b8fe56355689d70ab9b070af4ecaaceb12a16
Century300 <67885281+Century300@users.noreply.github.com>
2021-11-28 15:48:11 -0600

    Update install_powerlevel10k.sh

--- install_powerlevel10k.sh
+++ install_powerlevel10k.sh
@@ -18,6 +18,11 @@ sudo apt update
 # Clone the powerlevel10k repository to oh-my-zsh directory
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Copy Meslo fonts .ttf files into local fonts directory and refresh fonts cache
+mkdir -p ~/.local/share/fonts
+cp ~/Downloads/powerlevel10k/Meslo_Font/*ttf ~/.local/share/fonts
+fc-cache -f -v
+
 # Comment out the current ZSH_THEME in $HOME/.zshrc to disable it
 sudo sed -i 's/ZSH_THEME=/#ZSH_THEME=/g' ~/.zshrc
 
@@ -27,5 +32,6 @@ echo '''
 ZSH_THEME="powerlevel10k/powerlevel10k"
 ''' >> ~/.zshrc
 
-echo ''' You need to run "source ~/.zshrc" twice in your terminal to activate powerlevel10k or type "p10k configure".'''
+echo '''The scripts automatically installed the powerlevel10k official fonts MesloLGS, but you would need to manually change the font to MesloLGS Regular in your terminal profile under Edit-> Profiles...
+After that, you need to run "source ~/.zshrc" twice in your terminal to activate powerlevel10k or type "p10k configure".'''
 
